bf8696d70696b30a092379445dbc5f6e7c99cd75
Sharky! <jdeurt@gmail.com>
2018-03-22 14:26:25 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,21 @@
 <?php
-
+    public static function deleteDir($dirPath) {
+        if (! is_dir($dirPath)) {
+            throw new InvalidArgumentException("$dirPath must be a directory");
+        }
+        if (substr($dirPath, strlen($dirPath) - 1, 1) != '/') {
+            $dirPath .= '/';
+        }
+        $files = glob($dirPath . '*', GLOB_MARK);
+        foreach ($files as $file) {
+            if (is_dir($file)) {
+                self::deleteDir($file);
+            } else {
+                unlink($file);
+            }
+        }
+        rmdir($dirPath);
+    }
     function getIP() {
         if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
             $ip = $_SERVER['HTTP_CLIENT_IP'];
@@ -75,8 +91,6 @@
         - (str) url
     */
 
-    //Delete when done, turn this into a for loop later
-    unlink($storage_dir . '_LOG_ID_');
-    rmdir(substr($storage_dir, 0, -1));
+    deleteDir($storage_dir);
 
 ?>
